80d2af6826cc4c3da4649d0060efef06ed2d8185
酒井友太郎 <>
2021-07-14 23:30:02 +0900

    boardモデルの作成/board new & createアクション機能を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,4 +4,22 @@ class BoardsController < ApplicationController
 
     end
 
+    def new
+        @board = current_user.boards.build #フォームに空の初期値を渡す
+    end
+
+    def create
+        @board = current_user.boards.build(board_params) #空の箱にフォームからのデータを入れる
+        if @board.save
+            redirect_to boards_path(@board), notice: '保存ができました'
+        else
+            flash.now[:error] = '保存できませんでした'
+            render :new
+        end
+    end
+
+    private
+    def board_params #パラメータを引き渡すためのコード
+        params.require(:board).permit(:title, :content)
+    end
 end
